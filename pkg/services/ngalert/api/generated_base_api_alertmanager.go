1874c13e2c5b528e5c587e8724c2e5d22c55b3dc
George Robinson <george.robinson@grafana.com>
2021-07-30 18:07:20 +0100

    Create TestReceivers in Alertmanager interface

--- pkg/services/ngalert/api/generated_base_api_alertmanager.go
+++ pkg/services/ngalert/api/generated_base_api_alertmanager.go
@@ -32,7 +32,7 @@ type AlertmanagerApiService interface {
 	RouteGetSilences(*models.ReqContext) response.Response
 	RoutePostAMAlerts(*models.ReqContext, apimodels.PostableAlerts) response.Response
 	RoutePostAlertingConfig(*models.ReqContext, apimodels.PostableUserConfig) response.Response
-	RoutePostReceiversTest(*models.ReqContext, apimodels.TestReceiversConfig) response.Response
+	RoutePostTestReceivers(*models.ReqContext, apimodels.TestReceiversConfig) response.Response
 }
 
 func (api *API) RegisterAlertmanagerApiEndpoints(srv AlertmanagerApiService, m *metrics.Metrics) {
@@ -145,7 +145,7 @@ func (api *API) RegisterAlertmanagerApiEndpoints(srv AlertmanagerApiService, m *
 			metrics.Instrument(
 				http.MethodPost,
 				"/api/alertmanager/{Recipient}/config/api/v1/receivers/test",
-				srv.RoutePostReceiversTest,
+				srv.RoutePostTestReceivers,
 				m,
 			),
 		)
